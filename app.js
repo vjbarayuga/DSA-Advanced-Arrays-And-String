7df84112a3495241cea2a92ba4878425fe696be3
vjdbarayuga <vjdbarayuga@gmail.com>
2024-10-26 21:13:07 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const stringArr = ["a", "b", "c", "d"];
+console.log(stringArr[1]);
+
+const numArr = [1, 2, 3];
+numArr.push(4);
+console.log(numArr);
+
+numArr.pop();
+console.log(numArr);
+
+numArr.shift();
+console.log(numArr);
+
+numArr.unshift(1);
+console.log(numArr);
+
+const letters = ["a", "b", "d"];
+letters.splice(2, 1, "c");
+console.log(letters); 
+
